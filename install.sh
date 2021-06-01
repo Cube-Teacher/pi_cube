5ab379f7525cbc5210ca9909b3475bb997f8e766
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-06-01 17:30:36 +0800

    Update: add install script

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
 # install pupil_apriltags
+pip3 install pupil_apriltags
 
 # install processing
+curl https://processing.org/download/install-arm.sh | sudo sh
+
+# copy peasyCam to library
+cp ./lib/peasycam /home/pi/sketchbook/libraries
